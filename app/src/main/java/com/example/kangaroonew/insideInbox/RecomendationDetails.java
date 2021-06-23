4d8c3e423ba41793212de9f66fcf901d32868625
mushi <jmushi175@gmail.com>
2021-06-23 15:22:09 +0300

    recommendation and tips screens added

---
+++ app/src/main/java/com/example/kangaroonew/insideInbox/RecomendationDetails.java
@@ -0,0 +1,14 @@
+package com.example.kangaroonew.insideInbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecomendationDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recomendation_details);
+    }
+}
---
+++ app/src/main/java/com/example/kangaroonew/insideInbox/TipsDetails.java
@@ -0,0 +1,14 @@
+package com.example.kangaroonew.insideInbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TipsDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tips_details);
+    }
+}
